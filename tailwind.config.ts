6141ab964b928d2202ea6d0cfefb2e404b906c8a
nicklllson <nikita.vasia2015@gmail.com>
2024-07-15 22:41:24 +0500

    f

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -39,7 +39,7 @@ const config: Config = {
       'xl': {'max': '1279px'},
       'lg': {'max': '1023px'},
       'md': {'max': '767px'},
-      'sm': {'max': '639px'},
+      'sm': {'max': '424px'},
     }
   },
   plugins: [],
