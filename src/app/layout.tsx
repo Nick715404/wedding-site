463174b49e48b5973bd7c81bc581933e14958cd8
nicklllson <nikita.vasia2015@gmail.com>
2024-07-18 12:13:08 +0500

    Обновил фавикон и опен граф фоточку

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,18 @@ import { forum } from "@/constants";
 export const metadata: Metadata = {
   title: "Свадьба Никиты и Татьяны",
   description: "Сайт-приглашение на свадьбу Никита и Татьяны Симоновых",
+  openGraph: {
+    type: "website",
+    url: "https://simonovs-wedding.vercel.app/",
+    title: 'Свадьба Никиты и Татьяны',
+    description: 'Сайт-приглашение на свадьбу Никита и Татьяны Симоновых',
+    images: {
+      url: '/images/grap-img.jpg',
+      width: 1200,
+      height: 630,
+      alt: 'Свадьба Никиты и Татьяны',
+    },
+  }
 };
 
 type RootLayoutProps = { children: Readonly<React.ReactNode>, };
